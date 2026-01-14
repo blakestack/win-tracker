72af44e844b6b140390d1fe20db1a29e576bdeef
blakestack <66808440+blakestack@users.noreply.github.com>
2026-01-14 16:12:59 -0500

    Set up a class to call the cfbd api

--- whosontop/src/app/api/teams/route.ts
+++ whosontop/src/app/api/teams/route.ts
@@ -1,21 +1,23 @@
-import { client, getTeams } from 'cfbd';
+import { client, getTeams } from "cfbd";
 
 // Set up the client with your API key
 client.setConfig({
-    headers: {
-        'Authorization': 'Bearer F7w1YTndednOuQmCjJV8/dK2ZXgwR3hxs9meOQYpwoqt7nnCRNbZ60RMouP0P9JS',
-    }
+  headers: {
+    Authorization:
+      "Bearer F7w1YTndednOuQmCjJV8/dK2ZXgwR3hxs9meOQYpwoqt7nnCRNbZ60RMouP0P9JS",
+  },
 });
 
 // Call the getGames endpoint
 const teams = await getTeams({
-    query: {
-        year: 2025,
-        classification: 'fbs',
-    },
+  query: {
+    year: 2025,
+    classification: "fbs",
+  },
 });
 
 for (const team of teams.data ?? []) {
-    // Do something with the game data
-    // For example:
-    console.log(`${teams}`);
+  // Do something with the game data
+  // For example:
+  console.log(`${teams}`);
+}
